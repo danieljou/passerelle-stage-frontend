44b2faab020273aaf927b6ff4d3b272a4cb8b77b
danieljou <danielsitepro@gmail.com>
2024-04-23 02:14:01 +0100

    DEVELOPER

--- src/utils/env.tsx
+++ src/utils/env.tsx
@@ -3,8 +3,8 @@
 // export const BACKEND_API_URL: string = "http://127.0.0.1:8000/";
 // export const BACKEND_API_URL: string = "http://127.0.0.1:8000";
 export const BACKEND_API_URL: string =
-	"https://passerelle-stage-backend.vercel.app/";
+	"https://stage-passerelle-backend.vercel.app/";
 export const BACKEND_API_URL2: string =
-	"https://passerelle-stage-backend.vercel.app";
+	"https://stage-passerelle-backend.vercel.app";
 // export const BACKEND_API_URL: string =
 // 	"https://welldoneplanet-backend.vercel.app/";
