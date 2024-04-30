5fcf86b05de6ef9453153ee1c4ec8e5c1ef5854d
danieljou <danielsitepro@gmail.com>
2024-04-30 12:37:10 +0100

    ssh

--- src/utils/env.tsx
+++ src/utils/env.tsx
@@ -2,8 +2,8 @@
 
 // export const BACKEND_API_URL: string = "http://127.0.0.1:8000/";
 // export const BACKEND_API_URL2: string = "http://127.0.0.1:8000";
-// export const BACKEND_API_URL: string =
-"https://stage-passerelle-backend.vercel.app/";
+export const BACKEND_API_URL: string =
+	"https://stage-passerelle-backend.vercel.app/";
 export const BACKEND_API_URL2: string =
 	"https://res.cloudinary.com/dva5mhgio/image/upload/v1";
 // export const BACKEND_API_URL: string =
